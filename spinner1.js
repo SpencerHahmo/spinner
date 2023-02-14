7bce5f4228710a7edbcd2f73ef6e36dcbc8c7791
Spencer Hahmo <schahmo@gmail.com>
2023-02-14 14:41:26 -0700

    Low fidelity spinner

---
+++ spinner1.js
@@ -0,0 +1,66 @@
+let timer = 200;
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, timer);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, timer += 200);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, timer += 200);
